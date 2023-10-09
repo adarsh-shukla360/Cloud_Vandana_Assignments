32ee546c99b3a76e0212c4ced6bb8fe61f448609
adarsh-shukla360 <adarshshukla0360@gmail.com>
2023-10-09 21:52:34 +0530

    first commit

---
+++ Pangram/src/PangramChecker.java
@@ -0,0 +1,35 @@
+public class PangramChecker {
+    public static void main(String[] args) {
+        String input = "The quick brown fox jumps over the lazy dog";
+        boolean isPangram = isPangram(input);
+        
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean isPangram(String input) {
+        input = input.toLowerCase();
+
+        boolean[] alphabetPresent = new boolean[26];
+        
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                int index = c - 'a';
+                alphabetPresent[index] = true;
+            }
+        }
+
+        for (boolean present : alphabetPresent) {
+            if (!present) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+}
---
+++ ShuffleArray/src/ArrayShuffler.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class ArrayShuffler {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        
+        System.out.println("Shuffled Array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+
+    public static void shuffleArray(int[] arr) {
+        int n = arr.length;
+        Random rand = new Random();
+
+        for (int i = n - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            
+            // Swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
